77d32fd49990098df1be480438248af698cced64
zaxchaxs <zaxchaxsecondid@gmail.com>
2026-04-14 09:31:15 +0700

    Pertemuan 5 hasil akhir fix

--- Pertemuan 5 Hasil Akhir/assets/index.js
+++ Pertemuan 5 Hasil Akhir/assets/index.js
@@ -1,6 +1,11 @@
-const hamburger = document.getElementById("hamburger");
+const detailButtons = document.querySelectorAll('.btn-detail');
 
-hamburger.addEventListener("click", () => {
-    console.log("Humberger Clicked!");
-    ;
+detailButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        document.getElementById('detailTitle').innerText = this.dataset.title;
+        document.getElementById('detailDescription').innerText = this.dataset.description;
+        document.getElementById('detailDeadline').innerText = this.dataset.deadline;
+        document.getElementById('detailPriority').innerText = this.dataset.priority;
+        document.getElementById('detailStatus').innerText = this.dataset.status;
+    });
 });
